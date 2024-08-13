fc7aab8470ad324f1a2736a4025a393d3f1a2400
nurlan1221 <nbayramov592@gmail.com>
2024-08-13 09:45:45 -0700

    0dan proje

---
+++ src/main/java/az/orient/msshopproduct/MsShopProductApplication.java
@@ -0,0 +1,13 @@
+package az.orient.msshopproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsShopProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsShopProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/orient/msshopproduct/MsShopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package az.orient.msshopproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsShopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
